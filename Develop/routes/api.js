424000586cc90381deda4877f5887cee55d95797
Michael DiSanto <mdisanto928@gmail.com>
2021-07-06 20:56:28 -0400

    added router.post for api.js

---
+++ Develop/routes/api.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const Workout = require('../models/workout.js');
+
+router.post('/api/workouts', (req, res) => {
+    Workout.create({})
+    .then((dbWorkout) => {
+        res.json(dbWorkout);
+    })
+    .catch((err) => {
+        res.json(err);
+    });
+});
+
+router.put('/api/workouts')

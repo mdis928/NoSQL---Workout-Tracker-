0feeeb09e9ebc3e361bdaa6a19c45738e286c6c9
Michael DiSanto <mdisanto928@gmail.com>
2021-07-06 20:43:15 -0400

    added index.js in models

--- Develop/server.js
+++ Develop/server.js
@@ -15,7 +15,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useFindAndModify: false
 });
 
-// routes
+// routes - Make sure to uncomment 
 // app.use(require("./routes/api.js"));
 app.use(require("./routes/html"))
 

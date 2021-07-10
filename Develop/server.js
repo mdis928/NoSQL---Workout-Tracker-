063553b537f88a55349b4b731f5705f9c790a247
Michael DiSanto <mdisanto928@gmail.com>
2021-07-09 22:04:51 -0400

    deploying

--- Develop/server.js
+++ Develop/server.js
@@ -10,8 +10,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/first_db", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
